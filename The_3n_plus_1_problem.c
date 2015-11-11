6aacefb6627df4938cb33dcf5be47f6ae101cf32
RobinLIn <robin60207@gmail.com>
2015-11-11 09:50:56 +0800

    The_3n_plus_1_problem

---
+++ The_3n_plus_1_problem.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    while(n != 1) {
+        printf("%d ", n);
+        if((n%2) == 1) {
+            n = ((3*n)+1);
+        } else {
+            n = (n / 2);
+        }
+    }
+
+    printf("%d \n", n);
+
+    return 0;
+}
